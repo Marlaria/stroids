23d82942865fa2c66f8d51ab7229a9c5e7bf8f3c
marlaria <mbustamantedev@gmail.com>
2025-06-19 17:33:48 -0500

    added sprite groups, player movement controls

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -8,13 +9,31 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+
+    
     while True:
+        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for o in updateable:
+            o.update(dt)
+        for o in drawable:
+            o.draw(screen)
 
-        screen.fill(BLACK)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
