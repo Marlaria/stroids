f526eb215e5eed2a43f215756d4ae53806774944
marlaria <mbustamantedev@gmail.com>
2025-06-19 19:03:38 -0500

    added stroids

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 
 def main():
@@ -14,11 +16,14 @@ def main():
 
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    
+    field = AsteroidField()
 
     
     while True:
